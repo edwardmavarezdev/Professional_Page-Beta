add488eb858de49a32307051de7fc4c740504a62
edwardmavarezdev <edwardmavarezdev@gmail.com>
2021-10-07 21:30:38 -0400

    added observer for all projects bases

--- edwardmavarezdev.js
+++ edwardmavarezdev.js
@@ -1,5 +1,11 @@
-const project1 = document.getElementById("projectId1")
-const contact = document.querySelector('#projectId1')
+
+//project selector
+
+const project1 = document.querySelector('#projectId1')
+const project2 = document.querySelector('#projectId2')
+const project3 = document.querySelector('#projectId3')
+
+//observer options
 
 const options = {
     //root:,
@@ -7,9 +13,11 @@ const options = {
     threshold: 1
   }
 
-let observer = new IntersectionObserver(projectObserve1, options);
+//observer project 1
 
-function projectObserve1(entries,options){
+let observer1 = new IntersectionObserver(projectObserve1, options);
+
+function projectObserve1(entries,data){
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0.9) {
       entry.target.classList.add('in-viewport');
@@ -20,5 +28,40 @@ function projectObserve1(entries,options){
     }
   });
 }
-observer.observe(contact);
+observer1.observe(project1);
 
+
+//observer priject 2
+
+let observer2 = new IntersectionObserver(projectObserve2, options);
+
+function projectObserve2(entries,data){
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0.9) {
+      entry.target.classList.add('in-viewport');
+      project2.className = "project1"
+    } else {
+      entry.target.classList.remove('in-viewport');
+     
+    }
+  });
+}
+observer2.observe(project2);
+
+
+//observer priject 3
+
+let observer3 = new IntersectionObserver(projectObserve3, options);
+
+function projectObserve3(entries,data){
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0.9) {
+      entry.target.classList.add('in-viewport');
+      project3.className = "project1"
+    } else {
+      entry.target.classList.remove('in-viewport');
+     
+    }
+  });
+}
+observer3.observe(project3);
